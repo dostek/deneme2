230f0d58070d110717c733fc24520f5045b51618
İsmail KARAGOZ <ikaragoz@gmail.com>
2022-08-01 18:25:27 +0300

    Initial Commit

---
+++ deneme2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  deneme2
+//
+//  Created by Ismail KARAGOZ on 1.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
